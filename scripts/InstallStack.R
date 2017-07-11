65b849ad38ceb939fcaadaea0b8ac1acc75c71c2
Samuel Jenness <smjenness@gmail.com>
2017-07-10 21:46:17 -0400

    Install Stack file

---
+++ scripts/InstallStack.R
@@ -0,0 +1,10 @@
+
+## Update PrEP Race
+
+devtools::install_github("statnet/tergmLite")
+devtools::install_github("statnet/EpiModelHIV", ref = "prep-race")
+
+
+# Estimation/Reestimation ABC
+system("scp est/*.rda hyak:/gscratch/csde/sjenness/race/est")
+system("scp scripts/estimation/*.reestim.* hyak:/gscratch/csde/sjenness/race/")
