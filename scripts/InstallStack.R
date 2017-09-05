e8f2634efbd01aade6fac85ad8737f855c422604
smjenness <smjenness@gmail.com>
2017-09-05 12:31:52 -0400

    Update install stack

--- scripts/InstallStack.R
+++ scripts/InstallStack.R
@@ -4,25 +4,30 @@
 devtools::install_github("statnet/tergmLite")
 devtools::install_github("statnet/EpiModelHIV", ref = "prep-race")
 
-
-## 1. Estimation/Reestimation ABC
-system("scp est/*.rda hyak:/gscratch/csde/sjenness/race/est")
-system("scp scripts/estimation/*.reestim.* hyak:/gscratch/csde/sjenness/race/")
-
-system("scp hyak:/gscratch/csde/sjenness/race/data/*.rda data/")
-system("scp est/*.rda hyak:/gscratch/csde/sjenness/race/est")
-
-
-# Fitting on libra
+## 1. Fitting on libra
 system("scp scripts/estimation/01.setup.R libra:~/prace")
 system("scp scripts/estimation/02.estim.R libra:~/prace")
 
 system("scp libra:~/prace/est/*.rda est/")
 
-## 2. Burnin testing
 
-system("scp est/*.rda hyak:/gscratch/csde/sjenness/race/est")
-system("scp scripts/burnin/*.burn.* hyak:/gscratch/csde/sjenness/race/")
+## 2. Reestimation ABC
 
-system("scp hyak:/gscratch/csde/sjenness/race/data/*.rda data/")
+## --- rename hyak dir to prace --- ##
+
+system("scp est/*.rda hyak:/gscratch/csde/sjenness/prace/est")
+system("scp scripts/estimation/*.reestim.* hyak:/gscratch/csde/sjenness/prace/")
+
+system("scp hyak:/gscratch/csde/sjenness/prace/data/*.rda data/")
+system("scp est/*.rda hyak:/gscratch/csde/sjenness/prace/est")
+
+
+## 3. Burnin testing
+
+system("scp est/*.rda hyak:/gscratch/csde/sjenness/prace/est")
+
+system("scp scripts/burnin/*.burn.* hyak:/gscratch/csde/sjenness/prace/")
+
+system("scp hyak:/gscratch/csde/sjenness/prace/data/*.n*.rda data/")
+
 
