8cee38cfa1cbe07e3e716e84a88cbcb1d97ab675
smjenness <smjenness@gmail.com>
2017-09-06 15:30:00 -0400

    Add race-specific calculations

--- scripts/test.adhr.sim.R
+++ scripts/test.adhr.sim.R
@@ -23,9 +23,9 @@ sim <- function(med.dur = 365, cov = 0.25) {
   return(nprep)
 }
 
-med.dur <- 365
+med.dur <- 1155
 cov <- 0.40
-sims <- replicate(100, sim(med.dur = med.dur, cov = cov))
+sims <- replicate(300, sim(med.dur = med.dur, cov = cov))
 
 par(mar = c(3,3,1,1), mgp = c(2,1,0))
 plot(x = 1, y = 1, type = "n", ylim = c(0, 1), xlim = c(0, 3650))
@@ -34,6 +34,8 @@ for (j in 1:ncol(sims)) {
 }
 abline(h = 0.5, v = med.dur, lty = 2)
 text(1, 0.1, round(rowMeans(sims)[med.dur], 3))
+
+1 - rowMeans(sims)[337]
 
 med.dur
 do.rate <- 1-(2^(-1/med.dur))
@@ -45,6 +47,7 @@ cdf
 # Black
 1-(1-(1-2^(-1/406)))^337
 
+# White
 1-(1-(1-2^(-1/1155)))^337
 
 
